a32935318002fad3e30b6594586af3503c4c5936
Tolga BAYRAK <bayraktolga28@gmail.com>
2026-02-25 00:14:44 +0300

    project set

---
+++ BayrakBackend/src/main/java/com/bayraktolga/BayrakBackend/BayrakBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bayraktolga.BayrakBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayrakBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BayrakBackendApplication.class, args);
+	}
+
+}
---
+++ BayrakBackend/src/test/java/com/bayraktolga/BayrakBackend/BayrakBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayraktolga.BayrakBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayrakBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
